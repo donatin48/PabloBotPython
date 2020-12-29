e1ce9f73111b80b472e1387d58f782e81034ce7a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-29 21:25:25 +0100

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import random
-import cogs.commandsPablo
+from cogs import commandsPablo
+
 import datetime , time
 
 bot = commands.Bot(command_prefix="!")
